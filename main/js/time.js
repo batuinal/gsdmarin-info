7ebb053db48eaf642e34eb1b9272869da40848e2
batuinal <batuinal@umich.edu>
2015-05-20 15:00:57 -0400

    added time taking into consideration the daylight saving of istanbul

--- main/js/time.js
+++ main/js/time.js
@@ -1,40 +1,70 @@
-/*function displayTime(id){
+/*function displayTime(id, id2){
 
-var today = new Date();
-var dateNow = today.toDateString();
-var timeNow = today.toTimeString();
+    function padTime(n) {
+        return (n < 10) ? '0' + n : n;
+    }
+    
+    var date = new Date();
+    
+    var h_local = padTime(date.getHours());
+    var m_local = padTime(date.getMinutes());
+    var s_local = padTime(date.getSeconds());
+    
+    var utc = date.getTime() - (date.getTimezoneOffset() * 60000);
+    var newdate = new Date(utc + (2*360000));
 
-document.getElementById(id).innerHTML = timeNow;
-setTimeout('displayTime("' + id + '");','1000');
-}
-*/
+    
+    document.getElementById(id).innerHTML = 'Local Time: ' + h_local + ':' + m_local + ':' + s_local;
+    setTimeout(displayTime,1000,id,id2);
+}*/
 
-function displayTime(id)
+
+function displayTime(local_id, ist_id)
 {
-    date = new Date;
-    year = date.getFullYear();
-    month = date.getMonth();
-    months = new Array('January', 'February', 'March', 'April', 'May', 'June', 'Jully', 'August', 'September', 'October', 'November', 'December');
-    d = date.getDate();
-    day = date.getDay();
-    days = new Array('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday');
-    h = date.getHours();
-    if(h<10)
-    {
-        h = "0"+h;
+    
+    function DST(){
+        var today = new Date;
+        var yr = today.getFullYear();
+        var dst_start = new Date("March 31, "+yr+" 03:00:00"); // 2nd Sunday in March can't occur after the 14th
+        var dst_end = new Date("October 31, "+yr+" 04:00:00"); // 1st Sunday in November can't occur after the 7th
+        var day = dst_start.getDay(); // day of week of 14th
+        dst_start.setDate(31-day); // Calculate Last Sunday in March of this year
+        day = dst_end.getDay(); // day of the week of 7th
+        dst_end.setDate(31-day); // Calculate first Sunday in October of this year
+        if (today >= dst_start && today < dst_end){ //does today fall inside of DST period?
+            return true; //if so then return true
+        }
+        return false; //if not then return false
     }
-    m = date.getMinutes();
-    if(m<10)
-    {
-        m = "0"+m;
+    
+    function padTime(n) {
+        return (n < 10) ? '0' + n : n;
     }
-    s = date.getSeconds();
-    if(s<10)
-    {
-        s = "0"+s;
+    
+    var date = new Date();
+    
+    var h_local = padTime(date.getHours());
+    var m_local = padTime(date.getMinutes());
+    var s_local = padTime(date.getSeconds());
+    
+    var utc = date.getTime() + (date.getTimezoneOffset() * 60*1000);
+    
+    var newdate;
+    if (DST()){
+        newdate = new Date(utc + (3*60*60*1000));
     }
-    result = ''+days[day]+' '+months[month]+' '+d+' '+year+' '+h+':'+m+':'+s;
-    document.getElementById(id).innerHTML = result;
-    setTimeout('displayTime("'+id+'");','1000');
+    else{
+        newdate = new Date(utc + (2*60*60*1000))
+    }
+    
+    var h_ist = padTime(newdate.getHours());
+    var m_ist = padTime(newdate.getMinutes());
+    var s_ist = padTime(newdate.getSeconds());
+    
+    document.getElementById(local_id).innerHTML = 'Local Time: ' + h_local + ':' + m_local + ':' + s_local;
+    document.getElementById(ist_id).innerHTML = 'Turkey: ' + h_ist + ':' + m_ist + ':' + s_ist;
+    
+    setTimeout(displayTime,1000,local_id,ist_id);
     return true;
 }
+
