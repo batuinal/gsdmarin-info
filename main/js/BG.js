6b3d6bd288f8ceb1bfb8bea89f35143624bc566b
sgunbay <sgunbay@umich.edu>
2015-05-22 01:36:02 -0400

    JQuery is finally functional. Can't use local links.

--- main/js/BG.js
+++ main/js/BG.js
@@ -1,18 +1,22 @@
 $(function() {
+
   var today = new Date();
   var hourNow = today.getHours();
-  //document.write('<div class="header2"> \n <div>' + 'hellow' + '</div>\n</div>');
 
   if (hourNow > 18) {
-
-  $('.body').css('background-image', 'url("' + http://imgs.mi9.com/uploads/photography/4480/white-clouds-and-blue-sky_1600x1200_78556.jpg + '")');
-  }
+    document.write('<div class="header2"> \n <div>' + 'IF' + '</div>\n</div>');
+    $('.body').css('background-image', 'url(http://imgs.mi9.com/uploads/photography/4480/white-clouds-and-blue-sky_1600x1200_78556.jpg)');
+  } 
   else if (hourNow > 12) {
-  $('.body').css('background-image', 'url("' + http://www.naturewallpaper.eu/desktopwallpapers/sky/1366x768/after-sunset-sky-1366x768.jpg + '")');
-  }
+    document.write('<div class="header2"> \n <div>' + 'ELSEIF' + '</div>\n</div>');
+    $('.body').css('background-image', 'url(http://www.naturewallpaper.eu/desktopwallpapers/sky/1366x768/after-sunset-sky-1366x768.jpg)');
+  } 
   else{
-  $('.body').css('background-image', 'url(images/index_BG.jpeg)');
+    // Figure out how to give local links! //FIXME
+    var oldurl = $('.body').css('background-image');
+    $('.body').css('background-image', 'url(../images/index_BG.jpg');
+	var newurl = $('.body').css('background-image');
+    //document.write('<div class="header2"> \n <div>' + oldurl + ' --> ' + newurl + '</div>\n</div>');
   }
    
 });
-//url(images/index_BG.jpeg)
