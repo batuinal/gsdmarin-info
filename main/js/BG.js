7209465f1f0149e23f33b4cdc42c48755266baa9
batuinal <batuinal@umich.edu>
2015-05-22 13:59:56 -0400

    Got query for Background change to work. BG.js done!!!

--- main/js/BG.js
+++ main/js/BG.js
@@ -4,19 +4,20 @@ $(function() {
   var hourNow = today.getHours();
 
   if (hourNow > 18) {
-    document.write('<div class="header2"> \n <div>' + 'IF' + '</div>\n</div>');
-    $('.body').css('background-image', 'url(http://imgs.mi9.com/uploads/photography/4480/white-clouds-and-blue-sky_1600x1200_78556.jpg)');
-  } 
-  else if (hourNow > 12) {
-    document.write('<div class="header2"> \n <div>' + 'ELSEIF' + '</div>\n</div>');
-    $('.body').css('background-image', 'url(http://www.naturewallpaper.eu/desktopwallpapers/sky/1366x768/after-sunset-sky-1366x768.jpg)');
-  } 
-  else{
-    // Figure out how to give local links! //FIXME
-    var oldurl = $('.body').css('background-image');
-    $('.body').css('background-image', 'url(../images/index_BG.jpg');
-	var newurl = $('.body').css('background-image');
-    //document.write('<div class="header2"> \n <div>' + oldurl + ' --> ' + newurl + '</div>\n</div>');
+        $('.body').css('background-image', 'url(main/css/images/night.jpg)');
   }
-   
-});
+  else if (hourNow > 12) {
+        $('.body').css('background-image', 'url(main/css/images/lunch.jpg)');
+  }
+ 
+  else{
+        $('.body').css('background-image', 'url(main/css/images/morning.jpg');
+  }
+ });
+
+/* FOR DEBUGGING
+ var url = $('.body').css('background-image');
+ //document.write('<div class="header2"> \n <div>' + oldurl + ' --> ' + newurl + '</div>\n</div>');
+ IF YOU WANT TO USE URL
+ $('.body').css('background-image', 'url(http://imgs.mi9.com/uploads/photography/4480/white-clouds-and-blue-sky_1600x1200_78556.jpg)');
+ */
