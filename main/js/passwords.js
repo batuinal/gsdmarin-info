e55ed336c8e51d6b622fcff6c3ea4bd6780c3a05
batuinal <batuinal@umich.edu>
2015-05-24 23:28:33 -0400

    template for admin

--- main/js/passwords.js
+++ main/js/passwords.js
@@ -2,3 +2,5 @@ function loginForm() {
     document.myform.action = "http://cdn.crunchify.com/wp-content/uploads/code/Crunchify-LoginPage.html";
     document.myform.submit();
 }
+
+http://www.edcint.co.nz/misc/autologin/
