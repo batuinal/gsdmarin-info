6f8d2ba73e28cc84be3caef3ab5cab9461cdfd0a
sgunbay <sgunbay@umich.edu>
2015-05-26 17:48:37 -0400

    Renaming doesn't fix anything either, but still. =P

--- main.py
+++ main.py
@@ -5,16 +5,16 @@ import cgi
 from google.appengine.api import users
 import webapp2
 
-class MainPage(webapp2.RequestHandler):
+class LoginPage(webapp2.RequestHandler):
     def get(self):
         return self.redirect('/',permanent=True)
 
-class LoggedIn(webapp2.RequestHandler):
+class LoginChecker(webapp2.RequestHandler):
     def post(self):
 		return self.redirect('/main/index.html',permanent=True)
 
 app = webapp2.WSGIApplication([
-    ('/', MainPage),	
-    ('/loggedin', LoggedIn)
+    ('/', LoginPage),	
+    ('/loggedin', LoginChecker)
 ], debug=True)
 
