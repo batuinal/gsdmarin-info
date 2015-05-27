6a4c814c856a77bc1dd67b6db84fbec92bbd5427
sgunbay <sgunbay@umich.edu>
2015-05-27 19:58:17 -0400

    shit is happening

--- main.py
+++ main.py
@@ -5,13 +5,17 @@ import cgi
 from google.appengine.api import users
 import webapp2
 
+class FileUpload(webapp2.RequestHandler):
+	def get(self):
+		return self.redirect('/pages/upload.html')
+	def post(self):
+		#Parsing
+		#Create the HTML Document
+		#document.write(data data data);
+	
 class LoginPage(webapp2.RequestHandler):
     def get(self):
         return self.redirect('/pages/login.html')
-
-class LoginChecker(webapp2.RequestHandler):
-    def post(self):
-		return self.redirect('/pages/index.html')
 		
 class NotFoundHandler(webapp2.RequestHandler):
 	def get(self):
@@ -19,8 +23,8 @@ class NotFoundHandler(webapp2.RequestHandler):
 		self.response.write('<head><title>Page Not Found</title></head> \n <body><h1>Error 404 - Page Not Found.</h1></body>')
 
 app = webapp2.WSGIApplication([
-    ('/', LoginPage),	
-    ('/loggedin', LoginChecker),
+    ('/', LoginPage),
+	('/upload', FileUpload),
 	('/*', NotFoundHandler)
 ], debug=True)
 
